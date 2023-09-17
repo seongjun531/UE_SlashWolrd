99e6993d9947e8e02a50a5e5faf43bdf26c50e3a
seongjun531 <98207011+seongjun531@users.noreply.github.com>
2023-09-18 00:15:02 +0900

    CPP test

--- Source/UE_SlashOpenWorld/Private/Items/Item.cpp
+++ Source/UE_SlashOpenWorld/Private/Items/Item.cpp
@@ -14,11 +14,23 @@ void AItem::BeginPlay()
 	Super::BeginPlay();
 	
 	UE_LOG(LogTemp, Warning, TEXT("Begin Play called!"));
+
+	if (GEngine) 
+	{
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString("Item OnScreen Message!"));
+	}
 }
 
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	UE_LOG(LogTemp, Warning, TEXT("DeltaTime : %f"), DeltaTime);
+
+	if (GEngine)
+	{
+		FString Message = FString::Printf(TEXT("DeltaTime: %f"), DeltaTime);
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, Message);
+	}
 }
 
